c1c9aa4f5005c0f099093cdcaf20948b4f9782b9
Parv VijayVargiya <parvdev0109@gmail.com>
2022-06-30 04:07:37 +0530

    step-10

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,8 +3,9 @@ import 'package:flutter/cupertino.dart';
 
 @immutable
 class AuthUser {
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
   final bool isEmailVerified;
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
